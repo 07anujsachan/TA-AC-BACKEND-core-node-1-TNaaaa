a4ef902ae180604623f9da6bdb2a6de471edf73c
anuj sachan <anujsachan98@gmail.com>
2023-06-15 23:02:04 +0530

    Explain with code

---
+++ block-BNaaad/index.js
@@ -0,0 +1,10 @@
+console.log("wlcome to node.js");
+function square(num) {
+  return num * num;
+}
+console.log(square(5));
+let sum = 0;
+for (let i = 0; i <= 10; i++) {
+  sum = sum + i;
+}
+console.log(sum);
